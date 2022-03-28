de2dd436fc43c6086b15510845d1be1762a446b9
yunsoe <yunsoe3105@gmail.com>
2022-03-28 16:32:47 +0800

    added foreign keys to relevant models

--- models/recycler_type.go
+++ models/recycler_type.go
@@ -2,6 +2,8 @@ package models
 
 type Recycler_Type struct {
 	ID       uint     `gorm:"auto_increment;primary_key" json:"id,omitempty"`
-	Recycler Recycler `json:"recycler,omitempty"`
-	Type     Type     `json:"type,omitempty"`
+	Recycler Recycler `gorm:"foreignkey:RecyclerID" json:"recycler,omitempty"`
+	Type     Type     `gorm:"foreignkey:TypeName" json:"type,omitempty"`
+	RecyclerID uint `json:"-"`
+	TypeName string `json:"-"`
 }
