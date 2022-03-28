916f6d54d1d25330711f39edc0779e38d7760439
yunsoe <yunsoe3105@gmail.com>
2022-03-28 15:38:37 +0800

    Update ownership.go

--- models/ownership.go
+++ models/ownership.go
@@ -3,20 +3,22 @@ package models
 import "time"
 
 type Ownership struct {
+	/*
 	ID              uint      `gorm:"auto_increment;primary_key" json:"id,omitempty"`
 	Owner           User      `json:"owner,omitempty"`
 	Device          Device    `json:"device,omitempty"`
 	SerialNumber    string    `json:"serial_number,omitempty"`
 	PurchaseDate    time.Time `json:"purchase_date,omitempty"`
 	ReceiptLocation string    `json:"receipt_location,omitempty"`
-	/*
+	*/
+	
 	ID              uint `gorm:"auto_increment;primary_key" json:"id,omitempty"`
-	Owner           User `gorm:"foreignkey:UserID"`
-	Device          Device `gorm:"foreignkey:DeviceID"`
-	SerialNumber    string    `json:"serial_number"`
-	PurchaseDate    time.Time `json:"purchase_date"`
-	ReceiptLocation string    `json:"receipt_location"`
+	Owner           User `gorm:"foreignkey:UserID" json:"owner,omitempty"`
+	Device          Device `gorm:"foreignkey:DeviceID" json:"device,omitempty"`
+	SerialNumber    string    `json:"serial_number,omitempty"`
+	PurchaseDate    time.Time `json:"purchase_date,omitempty"`
+	ReceiptLocation string    `json:"receipt_location,omitempty"`
 	UserID string `json:"-"`
 	DeviceID uint `json:"-"`
-	*/
+	
 }
